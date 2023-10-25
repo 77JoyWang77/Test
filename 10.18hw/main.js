43e20765a9d1515dc617b72c22f4fb076fcb7da9
77JoyWang77 <wangchaoyin77@gmail.com>
2023-10-25 20:40:01 +0800

    First commit

--- 10.18hw/main.js
+++ 10.18hw/main.js
@@ -1,5 +1,6 @@
 
 let myGraph4 = document.getElementById('myGraph4');
+let myGraph5 = document.getElementById('myGraph5');
 
 let trace1_4 = {};
 trace1_4.type = "pie";
@@ -11,11 +12,15 @@ trace1_4.domain = {
     row: 0,
     column: 0
 };
-trace1_4.color_discrete_map={
-    '中國大陸(含香港)':'lightcyan',
-    '東南亞國協(10國)':'cyan',
-    '美國':'royalblue',
-    'Sun':'darkblue'
+trace1_4.marker = {
+    colors : [
+        'rgb(193, 203, 215)',
+        'rgb(134, 150, 167)',
+        'rgb(181, 196, 177)',
+        'rgb(201, 192, 211)',
+        'rgb(238, 229, 248)',
+        'rgb(150, 164, 139)'
+    ]
 };
 
 for (let i = 0; i < export_country.length; i++) {
@@ -46,8 +51,18 @@ trace3_4.labels = [];
 trace3_4.values = [];
 trace3_4.hole = 0.5
 trace3_4.domain = {
-    row: 1,
+    row: 0,
     column: 0
+};
+trace3_4.marker = {
+    colors : [
+        'rgb(193, 203, 215)',
+        'rgb(134, 150, 167)',
+        'rgb(181, 196, 177)',
+        'rgb(201, 192, 211)',
+        'rgb(238, 229, 248)',
+        'rgb(150, 164, 139)'
+    ]
 };
 
 for (let i = 0; i < export_goods.length; i++) {
@@ -62,8 +77,16 @@ trace4_4.labels = [];
 trace4_4.values = [];
 trace4_4.hole = 0.5
 trace4_4.domain = {
-    row: 1,
+    row: 0,
     column: 1
+};
+trace4_4.marker = {
+    colors : [
+        'rgb(193, 203, 215)',
+        'rgb(150, 164, 139)',
+        'rgb(238, 229, 248)',
+        'rgb(201, 192, 211)'
+    ]
 };
 
 for (let i = 0; i < import_goods.length; i++) {
@@ -75,17 +98,30 @@ for (let i = 0; i < import_goods.length; i++) {
 let data4 = [];
 data4.push(trace1_4);
 data4.push(trace2_4);
-data4.push(trace3_4);
-data4.push(trace4_4);
+
+let data5 = [];
+data5.push(trace3_4);
+data5.push(trace4_4);
 
 let layout4 = {
     margin: {
         t: 50,
     },
     grid: {
-        rows: 2,
+        rows: 1,
         columns: 2
     },
-    title: '111年進出口貿易量'
+    title: '111年進出口貿易量—主要進出口國家'
+};
+let layout5 = {
+    margin: {
+        t: 50,
+    },
+    grid: {
+        rows: 1,
+        columns: 2
+    },
+    title: '111年進出口貿易量—主要進出口貨品'
 };
 Plotly.newPlot(myGraph4, data4, layout4);
+Plotly.newPlot(myGraph5, data5, layout5);
